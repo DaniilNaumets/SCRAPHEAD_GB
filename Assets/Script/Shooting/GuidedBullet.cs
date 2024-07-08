0a7b88f40490cd77ab514c795858b84f3418bb34
Daniil Naumets <naumetsdaniil@mail.ru>
2024-07-08 19:59:35 +0300

    -fails build

--- Assets/Script/Shooting/GuidedBullet.cs
+++ Assets/Script/Shooting/GuidedBullet.cs
@@ -1,8 +1,6 @@
 using Enemies;
 using Resources;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.EditorTools;
 using UnityEngine;
 
 public class GuidedBullet : Bullet
