be303e979f58b63c2750bd58160e37e1b3fd6c3b
Daniil Naumets <naumetsdaniil@mail.ru>
2024-07-10 12:43:38 +0300

    add mineGun prefab

--- Assets/Script/LaserDamage.cs
+++ Assets/Script/LaserDamage.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserDamage : Bullet
+public class LaserDamage : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
